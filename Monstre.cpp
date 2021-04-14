8a31449e0992d807a282e7ccb42d24b42710f53b
TomSachot <74669910+TomSachot@users.noreply.github.com>
2021-04-14 15:32:16 +0200

    Update Monstre.cpp

--- Monstre.cpp
+++ Monstre.cpp
@@ -1,1 +1,31 @@
-Monstre.cpp
+#ifndef MONSTRE_CPP
+#define MONSTRE_CPP
+
+#include "Monstre.h"
+#include <iostream>
+
+    Monstre::Monstre() :
+
+    void Monstre::attaqueMonstre1(){
+
+
+    }
+
+    bool Monstre::testVictoire(){
+
+
+    }
+
+    void Monstre::attaqueMonstre2(){
+
+
+    }
+
+    bool Monstre::testVictoire(){
+
+
+    }
+
+    void Monstre::finTour(){
+        
+    }
