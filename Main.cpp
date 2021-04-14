b3117cbf06ea78e60b47bb1dbd82b0238be61400
TomSachot <74669910+TomSachot@users.noreply.github.com>
2021-04-14 16:55:21 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,21 @@
-Main.cpp
+#include "Monstre.cpp"
+#include <iostream>
+#include <string>
+
+int main(){
+
+    bool partiegagnee = false;
+    Cartes jeu = Monstre();
+
+
+    while(!partiegagnee){
+        jeu.attaqueMonstre1;
+        partiegagnee = jeu.testVictoire();
+        jeu.attaqueMonstre2;
+        partiegagnee = jeu.testVictoire();
+        jeu.finTour;
+    }
+
+
+    return 0;
+}
